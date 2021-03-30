f612922f18a6e4ae01c62011c667f1461f97d5fc
David Claveau <daveclaveau@gmail.com>
2021-03-30 22:44:01 +0000

    Add more conditional examples again

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,16 @@
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+let temperature = 41;
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
 let raining = false;
 
 if (!raining) {
